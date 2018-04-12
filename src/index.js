d6fbf0b0df2a598c6d160791c16a01a52b719b11
Patrick Lin <patricklin@alumni.ubc.ca>
2018-04-12 13:00:39 -0700

    use puppeteer for dynamic content

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import axios from 'axios'
+import puppeteer from 'puppeteer'
 import cheerio from 'cheerio'
 import 'babel-polyfill'
 
 const scraper = async () => {
   try {
-    const response = await axios.get('')
-    const $ = cheerio.load(response.data)
-    console.log($.html())
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto('http://www.espn.com/soccer/scoreboard/_/league/FIFA.WORLDQ.AFC/date/20150312')
+
+    const textContent = await page.evaluate(() => {
+      return document.querySelector('#events').textContent
+    })
+
+    console.log(textContent)
+
+    browser.close()
   } catch (error) {
     console.log(error)
   }
